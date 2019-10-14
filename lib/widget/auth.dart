91563c240c33f7281a5761fe25ed2bae467ab76b
Cristhian Cantero <criis2015.clash@gmail.com>
2019-10-14 00:41:06 -0300

    Agregando la autentificacion de inicio de sesion y registro de usuarios con FireBase al proyecto de flutter.

---
+++ lib/widget/auth.dart
@@ -0,0 +1,19 @@
+import 'dart:async';
+import 'package:firebase_auth/firebase_auth.dart';
+
+abstract class BaseAuth {
+  Future<String> signInWithEmailAndPassword(String email, String password);
+  Future<String> createUserWithEmailAndPassword(String email, String password);
+}
+
+class Auth implements BaseAuth {
+  Future<String> signInWithEmailAndPassword(String correo, String contrasenia) async {
+    FirebaseUser user = (await FirebaseAuth.instance.signInWithEmailAndPassword(email: correo, password: contrasenia)).user;
+    return user.uid;
+  }
+
+  Future<String> createUserWithEmailAndPassword(String correo, String contrasenia) async {
+    FirebaseUser user = (await FirebaseAuth.instance.createUserWithEmailAndPassword(email: correo, password: contrasenia)).user;
+    return user.uid;
+  }
+}
--- lib/widget/registro.dart
+++ lib/widget/registro.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'auth.dart';
 import 'inicio_sesion.dart';
 
 class MyApp extends StatelessWidget {
@@ -6,193 +7,215 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return new MaterialApp(
       debugShowCheckedModeBanner: false,
-      routes: <String, WidgetBuilder>{
-        '/inicio_sesion' : (context) => LoginPage()
-      },
+
+      home: new SignUpPage(auth: new Auth()),
     );
   }
 }
 
 class SignUpPage extends StatefulWidget {
+  SignUpPage({this.auth});
+  final BaseAuth auth;
+
   @override
   State createState() => new SignUpPageState();
 }
 
 class SignUpPageState extends State<SignUpPage> {
+
+  final formKey = new GlobalKey<FormState>();
+
+  String _correo;
+  String _contrasenia;
+
+  bool validateAndSave(){
+    final form = formKey.currentState;
+    if (form.validate()) {
+      form.save();
+      return true;
+    }
+    return false;
+  }
+
+  void validateAndSubmit() async {
+    if (validateAndSave()) {
+      try {
+        String userId = await widget.auth.createUserWithEmailAndPassword(_correo, _contrasenia);
+        print('Se registró: $userId');
+      }
+      catch (e) {
+        print ('Error: $e');
+      }
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
       resizeToAvoidBottomPadding: false,
-      body: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: <Widget>[
-          Container(
-            child: Stack(
-              children: <Widget>[
-                Container(
-                  padding: EdgeInsets.fromLTRB(30.0, 80.0, 0.0, 0.0),
-                  child: Text(
-                    'Regístrate',
-                    style: TextStyle(
-                      fontSize: 50.0,
-                      fontWeight: FontWeight.bold,
+      body: SingleChildScrollView(
+        child: Column(
+          children: <Widget>[
+            Container(
+              child: Stack(
+                children: <Widget>[
+                  Container(
+                    padding: EdgeInsets.fromLTRB(30.0, 80.0, 0.0, 0.0),
+                    child: Text(
+                      'Regístrate',
+                      style: TextStyle(
+                        fontSize: 50.0,
+                        fontWeight: FontWeight.bold,
+                      ),
                     ),
                   ),
-                ),
-                Container(
-                  padding: EdgeInsets.fromLTRB(260.0, 80.0, 0.0, 0.0),
-                  child: Text(
-                    '.',
-                    style: TextStyle(
-                      fontSize: 50.0,
-                      fontWeight: FontWeight.bold,
-                      color: Colors.green,
+                  Container(
+                    padding: EdgeInsets.fromLTRB(260.0, 80.0, 0.0, 0.0),
+                    child: Text(
+                      '.',
+                      style: TextStyle(
+                        fontSize: 50.0,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.green,
+                      ),
                     ),
                   ),
-                ),
-              ],
+                ],
+              ),
             ),
-          ),
-          //Container para los input
-          Container(
-            padding: EdgeInsets.only(top: 20.0, left: 20.0, right: 20.0),
-            child: Column(
-              children: <Widget>[
-                TextField(
-                  decoration: InputDecoration(
-                    labelText: 'Nombre',
-                    labelStyle: TextStyle(
-                      fontFamily: 'Montserrat',
-                      fontWeight: FontWeight.bold,
-                      color: Colors.grey,
-                    ),
-                    focusedBorder: UnderlineInputBorder(
-                      borderSide: BorderSide(color: Colors.green),
-                    )
-                  ),
-                ),
-                SizedBox(height: 10.0,),
-                TextField(
-                  decoration: InputDecoration(
-                      labelText: 'Apellido',
-                      labelStyle: TextStyle(
-                        fontFamily: 'Montserrat',
-                        fontWeight: FontWeight.bold,
-                        color: Colors.grey,
-                      ),
-                      focusedBorder: UnderlineInputBorder(
-                        borderSide: BorderSide(color: Colors.green),
-                      )
-                  ),
-                ),
-                SizedBox(height: 10.0,),
-                TextField(
-                  decoration: InputDecoration(
-                      labelText: 'Nombre del taller',
-                      labelStyle: TextStyle(
-                        fontFamily: 'Montserrat',
-                        fontWeight: FontWeight.bold,
-                        color: Colors.grey,
-                      ),
-                      focusedBorder: UnderlineInputBorder(
-                        borderSide: BorderSide(color: Colors.green),
-                      )
-                  ),
-                ),
-                SizedBox(height: 10.0,),
-                TextField(
-                  decoration: InputDecoration(
-                    labelText: 'Correo Electronico',
-                    labelStyle: TextStyle(
-                      fontFamily: 'Montserrat',
-                      fontWeight: FontWeight.bold,
-                      color: Colors.grey,
-                    ),
-                    focusedBorder: UnderlineInputBorder(
-                      borderSide: BorderSide(color: Colors.green),
-                    ),
-                  ),
-                  keyboardType: TextInputType.emailAddress,
-                ),
-                SizedBox(height: 10.0),
-                TextField(
-                  decoration: InputDecoration(
-                    labelText: 'Contraseña',
-                    labelStyle: TextStyle(
-                      fontFamily: 'Montserrat',
-                      fontWeight: FontWeight.bold,
-                      color: Colors.grey,
-                    ),
-                    focusedBorder: UnderlineInputBorder(
-                      borderSide: BorderSide(color: Colors.green),
-                    ),
-                  ),
-                  keyboardType: TextInputType.text,
-                  obscureText: true,
-                ),
-                SizedBox(height: 10.0,),
-                TextField(
-                  decoration: InputDecoration(
-                    labelText: 'Confirmar contraseña',
-                    labelStyle: TextStyle(
-                      fontFamily: 'Montserrat',
-                      fontWeight: FontWeight.bold,
-                      color: Colors.grey,
-                    ),
-                    focusedBorder: UnderlineInputBorder(
-                      borderSide: BorderSide(color: Colors.green),
-                    ),
-                  ),
-                  keyboardType: TextInputType.text,
-                  obscureText: true,
-                ),
-                SizedBox(height: 35.0),
-                Container(
-                  height: 55.0,
-                  child: Material(
-                    borderRadius: BorderRadius.circular(40.0),
-                    shadowColor: Colors.greenAccent,
-                    color: Colors.green,
-                    elevation: 7.0,
-                    child: GestureDetector(
-                      onTap: () {},
-                      child: Center(
-                        child: Text(
-                          'Regístrame',
-                          style: TextStyle(
-                            color: Colors.white,
-                            fontWeight: FontWeight.bold,
+            //Container para los input
+            Container(
+              padding: EdgeInsets.only(top: 20.0, left: 20.0, right: 20.0),
+              child: Form(
+                key: formKey,
+                child: Column(
+                  children: <Widget>[
+                    TextFormField(
+                      decoration: InputDecoration(
+                          labelText: 'Nombre',
+                          labelStyle: TextStyle(
                             fontFamily: 'Montserrat',
+                            fontWeight: FontWeight.bold,
+                            color: Colors.grey,
+                          ),
+                          focusedBorder: UnderlineInputBorder(
+                            borderSide: BorderSide(color: Colors.green),
+                          )
+                      ),
+                    ),
+                    SizedBox(height: 10.0,),
+                    TextFormField(
+                      decoration: InputDecoration(
+                          labelText: 'Apellido',
+                          labelStyle: TextStyle(
+                            fontFamily: 'Montserrat',
+                            fontWeight: FontWeight.bold,
+                            color: Colors.grey,
+                          ),
+                          focusedBorder: UnderlineInputBorder(
+                            borderSide: BorderSide(color: Colors.green),
+                          )
+                      ),
+                    ),
+                    SizedBox(height: 10.0,),
+                    TextFormField(
+                      decoration: InputDecoration(
+                          labelText: 'Nombre del taller',
+                          labelStyle: TextStyle(
+                            fontFamily: 'Montserrat',
+                            fontWeight: FontWeight.bold,
+                            color: Colors.grey,
+                          ),
+                          focusedBorder: UnderlineInputBorder(
+                            borderSide: BorderSide(color: Colors.green),
+                          )
+                      ),
+                    ),
+                    SizedBox(height: 10.0,),
+                    TextFormField(
+                      decoration: InputDecoration(
+                        labelText: 'Correo Electronico',
+                        labelStyle: TextStyle(
+                          fontFamily: 'Montserrat',
+                          fontWeight: FontWeight.bold,
+                          color: Colors.grey,
+                        ),
+                        focusedBorder: UnderlineInputBorder(
+                          borderSide: BorderSide(color: Colors.green),
+                        ),
+                      ),
+                      keyboardType: TextInputType.emailAddress,
+                      validator: (value) => value.isEmpty ? 'Completar campo' : null,
+                      onSaved: (value) => _correo = value,
+                    ),
+                    SizedBox(height: 10.0),
+                    TextFormField(
+                      decoration: InputDecoration(
+                        labelText: 'Contraseña',
+                        labelStyle: TextStyle(
+                          fontFamily: 'Montserrat',
+                          fontWeight: FontWeight.bold,
+                          color: Colors.grey,
+                        ),
+                        focusedBorder: UnderlineInputBorder(
+                          borderSide: BorderSide(color: Colors.green),
+                        ),
+                      ),
+                      keyboardType: TextInputType.text,
+                      obscureText: true,
+                      validator: (value) => value.isEmpty ? 'Completar campo' : null,
+                      onSaved: (value) => _contrasenia = value,
+                    ),
+                    SizedBox(height: 35.0),
+                    Container(
+                      height: 55.0,
+                      child: Material(
+                        borderRadius: BorderRadius.circular(40.0),
+                        shadowColor: Colors.greenAccent,
+                        color: Colors.green,
+                        elevation: 7.0,
+                        child: RaisedButton(
+                          onPressed: validateAndSubmit,
+                          child: Center(
+                            child: Text(
+                              'Regístrame',
+                              style: TextStyle(
+                                color: Colors.white,
+                                fontWeight: FontWeight.bold,
+                                fontFamily: 'Montserrat',
+                              ),
+                            ),
                           ),
                         ),
                       ),
                     ),
-                  ),
-                ),
-                SizedBox(height: 30.0,),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: <Widget>[
-                    InkWell(
-                      onTap: () {
-                        Navigator.pop(context);
-                      },
-                      child: Text(
-                        'Volver',
-                        style: TextStyle(
-                          color: Colors.green,
-                          fontFamily: 'Montserrat',
-                          fontWeight: FontWeight.bold,
-                          decoration: TextDecoration.underline,
+                    SizedBox(height: 30.0,),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: <Widget>[
+                        InkWell(
+                          onTap: () {
+                            Navigator.pop(context);
+                          },
+                          child: Text(
+                            'Volver',
+                            style: TextStyle(
+                              color: Colors.green,
+                              fontFamily: 'Montserrat',
+                              fontWeight: FontWeight.bold,
+                              decoration: TextDecoration.underline,
+                            ),
+                          ),
                         ),
-                      ),
-                    )
+                      ],
+                    ),
+                    SizedBox(height: 30.0)
                   ],
                 ),
-              ],
+              ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
